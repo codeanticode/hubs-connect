0645b6e4b4e8c1eb0dad706e1f7230817a816e73
codeanticode <andres.colubri@gmail.com>
2022-06-05 07:36:40 -0400

    added setScene method

--- src/template/library/HelloLibrary.java
+++ src/template/library/HelloLibrary.java
@@ -4,6 +4,8 @@ package template.library;
 import processing.core.*;
 import avn.portal.*;
 
+import java.util.Map;
+import static java.util.Map.entry;
 import java.net.URL;
 
 /**
@@ -29,6 +31,12 @@ public class HelloLibrary {
 	avn.portal.ReticulumConnection connection;
 	avn.portal.RoomConnection room;
 
+	URL HUB_SERVER;
+
+	String userName;
+	String roomId;
+	String authToken;
+
 	/**
 	 * a Constructor, usually called in the setup() method in your sketch to
 	 * initialize and start the Library.
@@ -38,6 +46,13 @@ public class HelloLibrary {
 	 */
 	public HelloLibrary(PApplet theParent) {
 		myParent = theParent;
+
+		try {
+		    HUB_SERVER = new URL("https://hubs.mozilla.com");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
 		welcome();
 	}
 	
@@ -57,32 +72,51 @@ public class HelloLibrary {
         // https://medium.com/google-developer-experts/from-java-to-kotlin-and-back-i-java-calling-kotlin-9abfc6496b04
 		// https://livebook.manning.com/book/the-joy-of-kotlin/a-mixing-kotlin-with-java/v-8/226
 
-		try {
-			URL HUB_SERVER = new URL("https://hubs.mozilla.com");
+		this.userName = userName;
+		this.roomId = roomId;
+		this.authToken = authToken;
 
-			connection = new avn.portal.ReticulumConnection(HUB_SERVER, authToken);
-			room = new avn.portal.RoomConnection(roomId, connection, userName);
+        try {
+            connection = new avn.portal.ReticulumConnection(HUB_SERVER, authToken);
+            room = new avn.portal.RoomConnection(roomId, connection, userName);
 			
-			if (room.isOpen()) {
-				System.out.println("SUCCESS to open room");
-			}
+            if (room.isOpen()) {
+                System.out.println("SUCCESS to open room");
+            }
 
-			return true;
-		} catch (Exception ex) {
-			System.err.println("Hubs connection failed");
-			ex.printStackTrace();
-		}
-		return false;
+            return true;
+        } catch (Exception ex) {
+            System.err.println("Hubs connection failed");
+            ex.printStackTrace();
+        }
+
+        return false;
 	}
 
 	public void close() {
-		try {
-			room.close();
-			System.out.println("SUCCESS to close room");			
-		} catch (Exception ex) {
-			System.err.println("Hubs connection failed");
-			ex.printStackTrace();
-		}
+        try {
+            room.close();
+            System.out.println("SUCCESS to close room");			
+        } catch (Exception ex) {
+            System.err.println("Hubs connection failed");
+            ex.printStackTrace();
+        }
+	}
+
+	public boolean setScene(String sceneUrl) {
+        try {
+
+            // avn.portal.HubsApi.sendMessage(HUB_SERVER, authToken, roomId, "update_scene", "url", sceneUrl, userName);
+
+            avn.portal.HubsApi.sendMessage(HUB_SERVER, authToken, roomId, "update_scene", Map.ofEntries(entry("url", sceneUrl)), userName);
+
+            return true;
+        } catch (Exception ex) {
+            System.err.println("Hubs connection failed");
+            ex.printStackTrace();
+        }
+
+		return false;
 	}
 
 	/**
